3559fb0c031310be20210957531595717fbaf318
Jacob M. Slone <jslone1993@gmail.com>
2015-02-19 16:37:08 -0500

    toggle between dvorak and qwerty with space

--- src/main.js
+++ src/main.js
@@ -5,8 +5,8 @@ var synthArgs = require('synthArgs.js');
 // my second degree fm synth
 var keyboard = new synth(synthArgs.modulators, synthArgs.adsr);
 
-// my dvorak key
-var keys = {
+// qwerty
+var keyQwerty = {
 	'A' : 47,
 	'S' : 48,
 	'D' : 49,
@@ -19,6 +19,24 @@ var keys = {
 	'SEMICOLON' : 56,
 	'APOSTROPHE' : 57
 };
+
+//dvorak
+var keysDvorak = {
+	'A' : 47,
+	'O' : 48,
+	'E' : 49,
+	'U' : 50,
+	'I' : 51,
+	'D' : 52,
+	'H' : 53,
+	'T' : 54,
+	'N' : 55,
+	'S' : 56,
+	'MINUS' : 57
+}
+
+var keyLayouts = [keyQwerty,keysDvorak];
+var keyCurrentLayout = 0;
 
 console.log(keyboard);
 
@@ -40,20 +58,20 @@ sald.scene = {
 		ctx.fillRect(0,0, ctx.width, ctx.height);
 	},
 	key:function(key, down) {
-		//synth.key(key,down);
 		if (key === "SPACE" && down) {
 			this.blink = 1.0;
+			keyCurrentLayout++;
 		}
-		console.log(key);
 		switch(key) {
 			case 'SPACE':
 				this.blink = 1.0;
 				break;
 			default:
+				var keys = keyLayouts[keyCurrentLayout % keyLayouts.length];
 				if(keys[key]) {
 					keyboard.setKey(keys[key],down);	
 				}
-				
+				break;
 		}
 	},
 	mouse:function(pos, button, down) {
